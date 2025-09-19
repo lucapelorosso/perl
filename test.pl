da78f53b009d40b0c3dc9f6896fdfac594fa9e82
Luca Pelorosso <lpelo2000@gmail.com>
2025-09-19 09:27:53 +0200

    LP20250919: test 1

--- test.pl
+++ test.pl
@@ -1,2 +1,3 @@
-#!usr/local/bin/Perl
-print “Ciao mondo!”;
+#Note:The template file will be copied to a new file. When you change the content of the template file you can create new file with this base content. 
+$aa = 'Ciao mondo!';
+print($aa)
