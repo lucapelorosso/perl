5a40358cda914ff830005ebd275aa4ed84fe7f10
Luca Pelorosso <lpelo2000@gmail.com>
2025-09-19 12:28:09 +0200

    LP20250919: scrittura su file

--- test.pl
+++ test.pl
@@ -1,3 +1,21 @@
-#Note:The template file will be copied to a new file. When you change the content of the template file you can create new file with this base content. 
-$aa = 'Ciao mondo!';
-print($aa)
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+print "Ciao dal tuo primo script Perl su macOS!\n";
+
+
+my $filename = "/private/tmp/prova.txt";
+print "Scrittura completata su $filename  scrittura (>)" ;
+# oppure in modalità aappend(>>)
+open(my $fh, '>', $filename) or die "Non posso aprire il file '$filename': $!";
+
+# Scrivi del contenuto nel file
+print $fh "Ciao, questo è un testo scritto con Perl!\n";
+print $fh "Seconda riga di esempio.\n";
+
+# Chiudi il file
+close($fh);
+
+print "Scrittura completata su $filename\n";
